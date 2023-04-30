4e099f4bc86d448616d0943b844a54ef0d93a173
Jagan Rajagopal <jaganrajagopal@outlook.com>
2023-04-30 21:40:56 +0530

    fixes for import reference of ops test py

--- ops_test.py
+++ ops_test.py
@@ -1,15 +1,15 @@
 from ops import *
-from calculation import *
+from calculation import calculate
 
 
 def test_add():
-    assert add(2,3) == 5
+    assert calculate.add(2,3) == 5
 
-def test_subtract():
-    assert subtract(2, 3) == -1
+def test_subtract():    
+    assert calculate.subtract(2, 3) == -1
 
 def test_multiply():
-    assert multiply(2, 3) == 6
+    assert calculate.multiply(2, 3) == 6
 
 def test_divide():
-    assert divide(10,5) == 2
+    assert calculate.divide(10,5) == 2
