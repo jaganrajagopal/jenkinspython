7f633987055368657870d54743ce9c2f7086ec8d
Jagan Rajagopal <jaganrajagopal@outlook.com>
2023-04-30 17:36:22 +0530

    adding unit testing for this application

--- calculation.py
+++ calculation.py
@@ -1,12 +1,22 @@
 def add(num1, num2):
+    print("result is ", num1, "and", num2, "is", num1+num2)
     return num1 + num2
 
+def subtract(x,y):
+    return x-y
+
+def multiply(x,y):
+    return x*y
+
+def divide(x,y):
+    return x/y
+
 # get input from user
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+#num1 = float(input("Enter the first number: "))
+#num2 = float(input("Enter the second number: "))
 
 # perform addition 
-result = add(num1, num2)
+#result = add(num1, num2)
 
 # display result
-print("The sum of", num1, "and", num2, "is", result)
+
